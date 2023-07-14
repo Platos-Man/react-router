e4c2e227461f8044623cb54c232c5f08e7da0fb0
Platos-Man <joonasru@tutanota.com>
2023-07-14 11:09:09 +0300

    some css

--- vanlife/src/index.jsx
+++ vanlife/src/index.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styles from "./index.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <Link className="logo" to={"/"}>
+        #VANLIFE
+      </Link>
+      <Link className="link" to={"/"}>
+        Home
+      </Link>
+      <Link className="link" to={"/about"}>
+        About
+      </Link>
+    </div>
   );
 };
 
