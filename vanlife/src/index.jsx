c116223ecc41ef6407ffddcf0fc977d6963538ac
Platos-Man <joonasru@tutanota.com>
2023-07-14 11:49:52 +0300

    styling and pictures

--- vanlife/src/index.jsx
+++ vanlife/src/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import styles from "./index.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
+      <header>
+        <NavBar />
+      </header>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,24 +22,15 @@ const App = () => {
 
 const NavBar = () => {
   return (
-    <div className="navbar">
+    <>
       <Link className="logo" to={"/"}>
         #VANLIFE
       </Link>
-      <Link className="link" to={"/"}>
-        Home
-      </Link>
-      <Link className="link" to={"/about"}>
-        About
-      </Link>
-    </div>
+      <nav>
+        <Link to={"/about"}>About</Link>
+      </nav>
+    </>
   );
 };
 
-const Home = () => {
-  return <h1>Hello, React Router!</h1>;
-};
-const About = () => {
-  return <h1>This is a web page</h1>;
-};
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
