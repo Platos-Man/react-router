33a87c082baec7ecc680674083f86ea673536c7b
Platos-Man <joonasru@tutanota.com>
2023-07-14 08:54:55 +0300

    working base for router

--- vanlife/src/index.jsx
+++ vanlife/src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
+  return <h1>Hello, React Router!</h1>;
+};
+const About = () => {
+  return <h1>This is a web page</h1>;
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
