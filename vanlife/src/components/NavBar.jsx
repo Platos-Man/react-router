d4c9f6890b6191c2b31252cb0c0dfa9342f87717
Platos-Man <joonasru@tutanota.com>
2023-07-18 11:35:21 +0300

    added content for VanDetail page

--- vanlife/src/components/NavBar.jsx
+++ vanlife/src/components/NavBar.jsx
@@ -9,7 +9,6 @@ const NavBar = () => {
       <nav>
         <Link to={"/about"}>About</Link>
         <Link to={"/vans"}>Vans</Link>
-        <Link to={"/vans/1"}>pebis</Link>
       </nav>
     </>
   );
--- vanlife/src/pages/Vans.jsx
+++ vanlife/src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = useState([]);
@@ -23,15 +24,17 @@ const Vans = () => {
 const Van = ({ van }) => {
   return (
     <div className="van-tile">
-      <img src={van.imageUrl} />
-      <div className="van-info">
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
-      </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
   );
 };
