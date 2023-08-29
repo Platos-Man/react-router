3b74a5b9422fc8e84f932de9fc03724709d01b00
Platos-Man <joonasru@tutanota.com>
2023-08-29 13:07:56 +0300

    added stuff to Error component

--- vanlife/src/components/Error.jsx
+++ vanlife/src/components/Error.jsx
@@ -1,3 +1,11 @@
+import { useRouteError } from "react-router-dom";
+
 export default function Error() {
-  return <h1>ERROR ERROR ERROR</h1>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <h1>
+      {error.statusText}: {error.message}
+    </h1>
+  );
 }
