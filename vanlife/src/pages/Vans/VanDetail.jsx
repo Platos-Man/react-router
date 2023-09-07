787301e8b3d8d61d837d6c0d813faf2f2f75bbbd
Platos-Man <joonasru@tutanota.com>
2023-09-07 10:04:44 +0300

    removed unneeded code

--- vanlife/src/pages/Vans/VanDetail.jsx
+++ vanlife/src/pages/Vans/VanDetail.jsx
@@ -1,5 +1,4 @@
-import { useParams, Link, useLocation, useLoaderData } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link, useLocation, useLoaderData } from "react-router-dom";
 import { getVans } from "../../api";
 
 export const vanLoader = async ({ params }) => {
@@ -9,7 +8,6 @@ export const vanLoader = async ({ params }) => {
 const VanDetail = () => {
   const location = useLocation();
   const van = useLoaderData();
-  console.log(van);
 
   const search = location.state?.search || "";
   const type = location.state?.type || "all";
@@ -19,20 +17,17 @@ const VanDetail = () => {
       <Link to={`..${search}`} relative="path" className="back-button">
         &larr; <span>Back to {type} vans</span>
       </Link>
-      {van ? (
-        <div className="van-detail">
-          <img src={van.imageUrl} />
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-          <h2>{van.name}</h2>
-          <p className="van-price">
-            <span>${van.price}</span>/day
-          </p>
-          <p>{van.description}</p>
-          <button className="link-button">Rent this van</button>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
+
+      <div className="van-detail">
+        <img src={van.imageUrl} />
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        <h2>{van.name}</h2>
+        <p className="van-price">
+          <span>${van.price}</span>/day
+        </p>
+        <p>{van.description}</p>
+        <button className="link-button">Rent this van</button>
+      </div>
     </div>
   );
 };
